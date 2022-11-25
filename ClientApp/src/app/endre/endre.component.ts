76ca9fe876f4e5723b5f0847679fc701c4fa79c1
s349967 <s349967@oslomet.no>
2022-11-25 17:05:03 +0100

    Basic klient validering av endre og lagre skjema, med unntak av symptom og varighet liste
    Basic klient validering av endre og lagre skjema, med unntak av symptom og varighet liste

--- ClientApp/src/app/endre/endre.component.ts
+++ ClientApp/src/app/endre/endre.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, RouterModule, Router } from '@angular/router';
 
 @Component({
@@ -10,10 +10,31 @@ import { ActivatedRoute, RouterModule, Router } from '@angular/router';
 export class EndreComponent {
 
   public id: Number | undefined;
+  endreSchema: FormGroup;
 
-  constructor(private http: HttpClient, private fb: FormBuilder, private route: ActivatedRoute, private router : Router) { }
+  constructor(private http: HttpClient, private fb: FormBuilder, private route: ActivatedRoute, private router: Router) {
+    this.endreSchema = fb.group({
+
+      navn: [null, Validators.compose([
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9\s-]{3,40}$/),
+      ])],
+      beskrivelse: ["", Validators.compose([
+        Validators.required,
+        Validators.maxLength(700)
+      ])],
+      diagnoseId: ["", Validators.compose([
+        Validators.pattern(/^[0-9]{1,6}$/)
+      ])],
+      dypForklaring: ["", Validators.compose([
+        Validators.required,
+        Validators.maxLength(5000)
+      ])]
+    });
+  }
  
   ngOnInit() {
-    this.route.params.subscribe(params => {this.id = params.id })
+    this.route.params.subscribe(params => { this.id = params.id })
+
   }
 }
