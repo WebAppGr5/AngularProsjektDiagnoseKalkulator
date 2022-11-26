cadd5af5aa143ab76394074bbd13198caa37c16c
s349967 <s349967@oslomet.no>
2022-11-26 15:36:09 +0100

    ...
    ...

--- ClientApp/src/models/SymptomListModel.ts
+++ ClientApp/src/models/SymptomListModel.ts
@@ -4,7 +4,10 @@ export class SymptomListModel {
   beskrivelse: String;
   symptomId: Number;
 
+
   doHave: boolean = false;
+  varighetsValg: Number = 0;
+
   constructor(navn: String, beskrivelse: String, symptomGruppeId: Number, symptomId: Number) {
     this.navn = navn;
     this.beskrivelse = beskrivelse;
